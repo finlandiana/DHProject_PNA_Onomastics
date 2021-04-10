2ddad2a11d774feb2f1eaf386e4fb880735133d0
finlandiana <75732796+finlandiana@users.noreply.github.com>
2021-04-10 19:46:45 +0300

    Add files via upload

---
+++ Akk_Male_Names.R
@@ -0,0 +1,12 @@
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(readr)
+PNA_Data_xlsx <- read_excel("~/Asiakirjat/PNA_Data_xlsx.xlsx")
+View(PNA_Data_xlsx)
+
+#Selecting the wanted data
+Akk_Male_Names <- subset(PNA_Data_xlsx, NameOrigin == "Akkadian" & Gender == "masc")
+  select = c("Name", "Time", "Profession", "Place")
+
+  
